6ea009ac20d0f816bc72bbf1e4f6ca800c398077
Abisma <imabubakar508@gmail.com>
2024-02-26 09:48:09 +0300

    feat: add read function in the canister code

--- src/credmotoko_backend/main.mo
+++ src/credmotoko_backend/main.mo
@@ -1,5 +1,62 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Trie "mo:base/Trie";
+import Time "mo:base/Time";
+import Text "mo:base/Text";
+import Bool "mo:base/Bool";
+import Nat32 "mo:base/Nat32";
+
+// define an actor
+
+actor Credentials {
+  // credential identifier
+  public type CredentialID = Nat32;
+
+  // trie for credential
+  private func key(a : CredentialID) : Trie.Key<CredentialID> {
+    return { hash = a; key = a };
+  };
+
+  // credential type
+  public type Credential = {
+    id : Nat32;
+    issuer : Text;
+    recipient : Text;
+    details : Text;
+    issuedAt : Text;
+    validUntil : Text;
+    revoked : ?Bool;
+  };
+
+  // Forthcoming credential indentifier
+  private stable var next : CredentialID = 0;
+
+  // credential storage 
+
+  private stable var credentials : Trie.Trie<CredentialID, Credential> = Trie.empty();
+
+  //post credential
+
+  public func createCredential(credential : Credential) : async CredentialID {
+    let cid = next;
+    next += 1;
+
+
+    credentials := Trie.replace(
+      credentials,
+      key(credential.id),
+      Nat32.equal,
+      ?credential,
+    ).0;
+
+    return cid;
+  };
+
+  // fetch credential record
+
+  public query func read(id : Nat32) : async ?Credential {
+    let result = Trie.find(credentials, key(id), Nat32.equal);
+    return result;
   };
 };
+
+
+
--- src/credmotoko_frontend/src/App.jsx
+++ src/credmotoko_frontend/src/App.jsx
@@ -1,31 +1,96 @@
-import { useState } from 'react';
-import { credmotoko_backend } from 'declarations/credmotoko_backend';
+import React, { useState } from "react";
+import { credmotoko_backend } from "declarations/credmotoko_backend";
 
-function App() {
-  const [greeting, setGreeting] = useState('');
+const IdInput = () => {
+  const [credentialInfo, setCredentialInfo] = useState({
+    name: "",
+    issuedAt: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    credmotoko_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsButtonDisabled(true);
+  
+    try {
+      const id = document.getElementById("id").value;
+  
+      const response = await credmotoko_backend.read(parseInt(id, 10));
+  
+      if (response) {
+        const { name, issuedAt } = response;
+        console.log(response)
+  
+        setCredentialInfo({
+          name,
+          issuedAt,
+        });
+  
+        setErrorMessage("");
+      } else {
+        setErrorMessage("Credential Identification number not found");
+        setCredentialInfo({
+          name: "",
+          issuedAt: "",
+        });
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setErrorMessage("Error fetching data. Please try again.");
+    } finally {
+      setIsButtonDisabled(false);
+    }
+  };
+  
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="code">Credential Idenfitication Number (CID):</label>
+        <input
+          type="text"
+          id="id"
+          required
+          placeholder="Input credential ID"
+          style={{
+            width: 200,
+            padding: 8,
+            margin: 10,
+            border: "1px solid #ccc",
+            borderRadius: 4,
+            fontSize: 14,
+          }}
+        />
 
-export default App;
+        <button
+          type="submit"
+          disabled={isButtonDisabled}
+          style={{
+            backgroundColor: "skyblue",
+            paddingLeft: 20,
+            paddingRight: 20,
+            paddingTop: 10,
+            paddingBottom: 10,
+            border: "none",
+            borderRadius: 5,
+            fontWeight: "bold",
+          }}
+        >
+          Submit
+        </button>
+      </form>
+
+      <div className="services-item">
+        <h1>Result output</h1>
+        <h4 style={{ color: errorMessage ? "red" : "green" }}>
+          {errorMessage}
+        </h4>
+        <h3>{credentialInfo.name}</h3>
+        <h3>{credentialInfo.issuedAt}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default IdInput;
